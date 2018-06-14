acbec7da98c14a438d082facc2736f26a88e8b05
Yuhan Wang <35240477+yw2436@users.noreply.github.com>
2018-06-14 12:58:29 -0400

    Update Simulation.sh

--- Read Simulation/Simulation.sh
+++ Read Simulation/Simulation.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 # I used chr1 of hg38 as the sorce sequence. 
 #SNP information is from UCSC Gene Browser and filtered that it only contains info about chr1 and every line has 26 columns. 
 #FASTQ info is from SRR1534766.
