26e1bbf2389919e324e82f368c241aac1fafda30
Yuhan Wang <35240477+yw2436@users.noreply.github.com>
2018-07-12 15:38:04 -0400

    Update whole pipeline.sh

--- RNAmapper/whole pipeline.sh
+++ RNAmapper/whole pipeline.sh
@@ -16,3 +16,12 @@ STAR --runMode alignReads --runThreadN 16 --genomeDir genome/GRCz10/ --readFiles
 STAR --runMode alignReads --runThreadN 16 --genomeDir genome/GRCz10/ --readFilesIn reads/wt_fasta/Miller_hox80.fastq  --outFileNamePrefix reads/STAR_wt/hox80
 STAR --runMode alignReads --runThreadN 16 --genomeDir genome/GRCz10/ --readFilesIn reads/wt_fasta/Miller_nhslMUT.fastq --outFileNamePrefix reads/STAR_wt/nhslMUT
 
+samtools mpileup -ABuf /mnt/bioinfo/RNAmapper_test2/genome/GRCz10/Danio_rerio.GRCz10.dna.toplevel.fa /mnt/bioinfo/RNAmapper_test2/reads/STAR_wt/nhslwt_sorted.bam | bcftools view -O b --threads 8 > /mnt/bioinfo/RNAmapper_test2/reads/STAR_wt/nhslwt_raw.bcf 
+samtools mpileup -ABuf /mnt/bioinfo/RNAmapper_test2/genome/GRCz10/Danio_rerio.GRCz10.dna.toplevel.fa /mnt/bioinfo/RNAmapper_test2/reads/STAR_wt/wt80_sorted.bam | bcftools view -O b --threads 8 > /mnt/bioinfo/RNAmapper_test2/reads/STAR_wt/wt80_raw.bcf 
+samtools mpileup -ABuf /mnt/bioinfo/RNAmapper_test2/genome/GRCz10/Danio_rerio.GRCz10.dna.toplevel.fa /mnt/bioinfo/RNAmapper_test2/read/STAR_mut/hox80_sorted.bam | bcftools view -O b --threads 8 > /mnt/bioinfo/RNAmapper_test2/reads/STAR_mut/hox80_raw.bcf 
+samtools mpileup -ABuf /mnt/bioinfo/RNAmapper_test2/genome/GRCz10/Danio_rerio.GRCz10.dna.toplevel.fa /mnt/bioinfo/RNAmapper_test2/read/STAR_mut/nhslmut_sorted.bam | bcftools view -O b --threads 8 > /mnt/bioinfo/RNAmapper_test2/reads/STAR_mut/nhslmut_raw.bcf   ##--outSAMstrandField intronMotif 
+
+for i in {1..25}; do bcftools view nhslwt_raw.bcf | grep -w ^$i > nhslwt_chr$i.vcf; done &
+for i in {1..25}; do bcftools view wt80_raw.bcf | grep -w ^$i > wt80_chr$i.vcf; done &
+for i in {1..25}; do bcftools view hox80_raw.bcf  | grep -w ^$i > hox80_chr$i.vcf; done &
+for i in {1..25}; do bcftools view nhslmut_raw.bcf  | grep -w ^$i > nhslmut_chr$i.vcf; done &
